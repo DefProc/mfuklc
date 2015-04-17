60e080152e7ba74ad989eb928e5a555cd8227eae
Patrick Fenner <contact@def-proc.co.uk>
2015-04-17 18:20:40 +0100

    analog based piezo checking, along with 1ms timer interrupt

--- range/arduino/piezo_score_test/piezo_score_test.ino
+++ range/arduino/piezo_score_test/piezo_score_test.ino
@@ -1,31 +1,48 @@
-/* Records inputs from a piezo knock sensor and displays cumulative hit count */
+/* Records inputs from a piezo knock sensor and displays cumulative hit count 
+  
+   Adjust the sensitivity of the hit based on the SCORE_THRESHOLD
+   A weight secured to the back of the piezo increases hit response e.g. an M6 
+   nut pressed on with blue tack.
+*/
+
+#include<TimerOne.h>
 
 #define BAUD 9600
-#define RESEND_TIMER 100
-#define DEBOUNCE_LIMIT 100
-#define IN INT0
+#define RESEND_TIMER 250
+#define DEBOUNCE_LIMIT 20
+#define SCORE_THRESHOLD 102
 
 unsigned long last_send = 0UL;
-volatile unsigned long last_strike = 0UL;
-volatile boolean report_score = false;
-volatile int counter = 0;
+boolean report_score = false;
+volatile unsigned long last_hit = 0UL;
+volatile boolean record_hit = false;
+volatile long counter = 0UL;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(IN, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  // open the USB serial port for reporting
+  Serial.begin(BAUD);
   
+  // open the scoreboard serial port
   Serial1.begin(BAUD);
   delay(100);
-  
   // reset the scoreboard to zero on startup
   Serial1.println(F("Z"));
   
-  attachInterrupt(IN, piezo, RISING);
+  // 1ms timer interrupt to check for hits
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(piezoCheck);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // record a hit if registered
+  if (record_hit == true) {
+    counter++;
+    Serial.println(F("HIT"));
+    record_hit = false;
+    report_score = true;
+  }
+  
+  // update the scoreboard if changed OR on regular resend time
   if (report_score == true || millis() - last_send >= RESEND_TIMER) {
     Serial1.print(F("D"));
     Serial1.println(counter);
@@ -34,10 +51,9 @@ void loop() {
   }
 }
 
-void piezo() {
-  if (millis() - last_strike >= DEBOUNCE_LIMIT) {
-    report_score = true;
-    counter++;
-    last_strike = millis();
+void piezoCheck() {
+  if (analogRead(A0) >= SCORE_THRESHOLD && millis() - last_hit >= DEBOUNCE_LIMIT) {
+    record_hit = true;
+    last_hit = millis();
   }
 }
