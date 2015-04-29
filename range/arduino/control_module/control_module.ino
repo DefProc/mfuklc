122ecf5696850307aeee907f054ef819e0248c07
Patrick Fenner <contact@def-proc.co.uk>
2015-04-29 10:22:38 +0100

    control moudule code, as at MFUK15

--- range/arduino/control_module/control_module.ino
+++ range/arduino/control_module/control_module.ino
@@ -3,28 +3,32 @@
 #include <Wire.h>
 #include <SL018.h>
 
+#define DEBUG 0
+
 #define BAUD 9600
-#define RESEND_TIMER 200
-#define RUN_TIMER 30000
+#define RESEND_TIMER 50
+#define RUN_TIMER 30250
 #define UPDATE_DELAY 10
 #define RFID_DIGITS 15
 #define BUTTON 2
 #define RESET 3
 #define LED 13
+#define RFID 4
 #define TARGET Serial1
 #define SCOREBD Serial2
 #define TIMER Serial3
 
 //states
 #define RFID_READY 1
-#define START_THE_GAME 2
-#define RUNNING 3
-#define CLEAN_UP 4
+#define WAIT_FOR_START 2
+#define START_THE_GAME 3
+#define RUNNING 4
+#define CLEAN_UP 5
 
 SL018 rfid;
 
 char player_rfid[RFID_DIGITS];
-unsigned long score = 0;
+unsigned long score = 0UL;
 int state = RFID_READY;
 unsigned long start_time = 0UL;
 unsigned long last_update = 0UL;
@@ -36,7 +40,10 @@ void setup() {
   SCOREBD.begin(BAUD); // Scoreboard module
   TIMER.begin(BAUD); // Timer module
   
-  rfid.seekTag();
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  pinMode(RESET, INPUT);
+  pinMode(RFID, INPUT_PULLUP);
 }
 
 void loop() {
@@ -45,8 +52,15 @@ void loop() {
     //waiting for new RFID scan
     stopall();
     // hold for rfid read
-    while (!rfid.available()); 
+    rfid.seekTag();
+    while (!rfid.available());
     strcpy(player_rfid, rfid.getTagString());
+#if DEBUG > 0 
+    Serial.print(F("rfid: "));
+    Serial.println(player_rfid);
+#endif    
+    state = WAIT_FOR_START;
+  } else if (state == WAIT_FOR_START) {
     sendPlayerID();
     resetall();
     while(digitalRead(BUTTON) == LOW && digitalRead(RESET) == LOW) {
@@ -54,13 +68,19 @@ void loop() {
       float val = (exp(sin(millis()/1000.0*PI)) - 0.36787944)*108.0;
       analogWrite(LED, val);
     } 
+    digitalWrite(LED, LOW);
     if (digitalRead(BUTTON) == HIGH) {
       state = START_THE_GAME;
+    } else {
+      state = RFID_READY;
     }
   } 
   
   else if (state == START_THE_GAME) {
     //set the initial conditions for play
+#if DEBUG > 0 
+    Serial.println(F("Start the game"));
+#endif    
     start();
     state = RUNNING;
   } 
@@ -84,6 +104,7 @@ void loop() {
     stopall();
     getScore();
     //report the <rfid>,<score> back to any listening device
+    Serial.print(F("0x"));
     Serial.print(player_rfid);
     Serial.print(',');
     Serial.print(score);
@@ -95,11 +116,12 @@ void loop() {
 void stopall() {
   TARGET.println(F("X"));
   TIMER.println(F("X"));
+  TIMER.println(F("Z"));
   SCOREBD.println(F("X"));
 }
 
 void resetall() {
-  SCOREBD.println(F("Z"));
+  SCOREBD.println(F("D0"));
   TIMER.println(F("R"));
   TARGET.println(F("X"));
 }
@@ -114,14 +136,18 @@ void sendPlayerID() {
 void getScore() {
   TARGET.println(F("D"));
   delay(UPDATE_DELAY); // need to wait for a reply
-  if (TARGET.available()) {
+//  if (TARGET.available()) {
     score = TARGET.parseInt();
-  }
+//  }
+#if DEBUG > 1
+  Serial.println(score);
+#endif
   SCOREBD.print(F("D"));
   SCOREBD.println(score);
 }
 
 void start() {
+  score = 0UL;
   //do the start
   //make a countdown on the scoreboard
   SCOREBD.println(F("D5"));
@@ -134,13 +160,13 @@ void start() {
   delay(1000);
   SCOREBD.println(F("D1"));
   delay(1000);
-  SCOREBD.println(F("D0"));
+  SCOREBD.println(F("Z"));
   delay(100);
   //start listening to the targets
   TARGET.println(F("S"));
+  TARGET.println(F("D"));
   //start the timer
   TIMER.println(F("S"));
   start_time = millis();
-  last_update = millis() - (2 * RESEND_TIMER);
-}
-  
+  last_update = millis() + RESEND_TIMER;
+}  
